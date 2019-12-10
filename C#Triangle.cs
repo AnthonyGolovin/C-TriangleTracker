afbf203051ee6d75883d139dbccd87a6e49a118d
Anthony Golovin and Wei Dai <anthony@>
2019-12-10 10:34:21 -0800

    create and finalize triangle project

--- C#Triangle.cs
+++ C#Triangle.cs
@@ -2,41 +2,60 @@ using System;
 using System.Threading;
 using System.Collections.Generic;
 
-// public class Triangle
-//     {
-//         public int Side1;
-//         public int Side2;
-//         public int Side3;
+public class Triangle
+    {
+        public int Side1Length;
+        public int Side2Length;
+        public int Side3Length;
 
 
-//         public Triangle(int side1, int side2, int side3)
-//         {
-//             Side1 = side1;
-//             Side2 = side2;
-//             Side3 = side3;
-//         }
-
-//         // public int calc()
-//         // {
-//         //     if (Side1 == Side3 && Side3 == Side2)
-//         //     {
-//         //         Console.WriteLine("This is an Isoceles Triangle");
-//         //     }
-//         //     // else if ()
-//         //     // {
-
-//         //     // }
-//         //     else if (Side1 != Side3 && Side1 != Side2 && Side2 != Side3)
-//         //     {
-//         //         Console.WriteLine("This is an Isoceles Triangle");
-//         //     }
+        public Triangle(int side1Length, int side2Length, int side3Length)
+        {
+            Side1Length = side1Length;
+            Side2Length = side2Length;
+            Side3Length = side3Length;
             
-//         // }
-//     }
+        }
+
+        public static string calc(int Side1Length, int Side2Length, int Side3Length)
+        {
+            int [] userSide = {Side1Length, Side2Length, Side3Length};
+            string Equilateral = "This is an Equilateral Triangle";
+            string Isosceles = "This is an Isosceles Triangle";
+            string Scalene = "This is a Scalene Triangle";
+            string NAT = "This is not a triangle";
+            string Invalid = "Please enter valid integers";
+
+        if (userSide[0] != userSide[1] && userSide[0] != userSide[2] && userSide[1] != userSide[2])
+            {
+                return Scalene;
+            }
+
+         else if (userSide[0] == userSide[1] && userSide[0] == userSide[2] && userSide[1] == userSide[2])
+            {
+                return Equilateral;
+            }
+
+         else if (userSide[0] + userSide[1] <= userSide[2] || userSide[1] + userSide[2] <= userSide[0] || userSide[2] + userSide[0] <= userSide[1])
+            {
+                return NAT;
+            }
+         else if (userSide[0] == userSide[1] && userSide[0] != userSide[2])
+            { 
+                return Isosceles;
+            }
+            else
+            {
+                return Invalid;
+            }
+            
+        }
+    }
 class Program 
 {
  static void Main()
     {
+       
         Console.WriteLine("Enter a side length for a Triangle.");
         int side1Length = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter the second side length for a Triangle.");
@@ -44,31 +63,8 @@ class Program
         Console.WriteLine("Enter the last side length for a Triangle.");
         int side3Length = int.Parse(Console.ReadLine());
 
-        // Triangle newTriangle = new Triangle(side1Length, side2Length, side3Length);
-        // int answer = newTriangle.calc();
-        // Console.WriteLine("Its is a " + answer + " triangle.");
-
-        int [] userSide = {side1Length, side2Length, side3Length};
-    
-
-        if (userSide[0] != userSide[1] && userSide[0] != userSide[2] && userSide[1] != userSide[2])
-            {
-                Console.WriteLine("This is an Scalene Triangle");
-            }
-
-         else if (userSide[0] == userSide[1] && userSide[0] == userSide[2] && userSide[1] == userSide[2])
-            {
-                Console.WriteLine("This is an Equilateral Triangle");
-            }
-
-         else if (userSide[0] + userSide[1] <= userSide[2] || userSide[1] + userSide[2] <= userSide[0] || userSide[2] + userSide[0] <= userSide[1])
-            {
-                Console.WriteLine("This is not a Triangle");
-            }
-         else if (userSide[0] == userSide[1] && userSide[0] != userSide[2])
-            {
-                Console.WriteLine("This is an Isoceles Triangle");
-            }
+        string answer = Triangle.calc(side1Length, side2Length, side3Length);
+        Console.WriteLine(answer);
 
             
     }
