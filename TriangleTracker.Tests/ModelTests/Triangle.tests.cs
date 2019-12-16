cf2650b792f653d5c006ecb172d04f6fbd49c181
Student <example@example.com>
2019-12-16 10:36:23 -0800

    Working on adding test methods

---
+++ TriangleTracker.Tests/ModelTests/Triangle.tests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleCode;
+
+
+namespace TriangleCode.Tests
+{
+  [TestClass]
+  public class CalcTest
+  {
+    [TestMethod]
+    public void Calc_TriangleTypeChecker_Scalene()
+    {
+        Triangle testTriangle = new Triangle(3,4,5);
+        // any necessary logic to prep for test; instantiating new classes, etc.
+        Assert.AreEqual("This is a Scalene Triangle", testTriangle.Calc(3,4,5));
+    }
+  }
+}
---
+++ TriangleTracker/Models/Triangle.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace TriangleCode {
+
+public class Triangle
+    {
+        public int Side1Length;
+        public int Side2Length;
+        public int Side3Length;
+
+
+        public Triangle(int side1Length, int side2Length, int side3Length)
+        {
+            Side1Length = side1Length;
+            Side2Length = side2Length;
+            Side3Length = side3Length;
+            
+        }
+
+        public static string Calc(int Side1Length, int Side2Length, int Side3Length)
+        {
+            int [] userSide = {Side1Length, Side2Length, Side3Length};
+            string Equilateral = "This is an Equilateral Triangle";
+            string Isosceles = "This is an Isosceles Triangle";
+            string Scalene = "This is a Scalene Triangle";
+            string NAT = "This is not a triangle";
+            string Invalid = "Please enter valid integers";
+
+        if (userSide[0] != userSide[1] && userSide[0] != userSide[2] && userSide[1] != userSide[2])
+            {
+                return Scalene;
+            }
+
+         else if (userSide[0] == userSide[1] && userSide[0] == userSide[2] && userSide[1] == userSide[2])
+            {
+                return Equilateral;
+            }
+
+         else if (userSide[0] + userSide[1] <= userSide[2] || userSide[1] + userSide[2] <= userSide[0] || userSide[2] + userSide[0] <= userSide[1])
+            {
+                return NAT;
+            }
+         else if (userSide[0] == userSide[1] && userSide[0] != userSide[2])
+            { 
+                return Isosceles;
+            }
+            else
+            {
+                return Invalid;
+            }
+            
+        }
+    }
+    class Program 
+    {
+    static void Main()
+        {
+        
+            Console.WriteLine("Enter a side length for a Triangle.");
+            int side1Length = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second side length for a Triangle.");
+            int side2Length = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the last side length for a Triangle.");
+            int side3Length = int.Parse(Console.ReadLine());
+
+            string answer = Triangle.Calc(side1Length, side2Length, side3Length);
+            Console.WriteLine(answer);
+
+                
+        }
+    }
+}
